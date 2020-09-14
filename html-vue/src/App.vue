73c6bd206af0d66f272d72c8602514c6a38ba183
fszaisl <fanshizhou@126.com>
2020-09-14 17:47:42 +0800

    123

--- html-vue/src/App.vue
+++ html-vue/src/App.vue
@@ -1,19 +1,19 @@
 <template>
-  <div>
-    <router-view />
-  </div>
+	<router-view />
 </template>
 
 <script>
 import { getBlogListUrl } from "./api";
 export default {
-  name: "app",
-  mounted: function () {
-    console.log(this);
-    this.axios.get(getBlogListUrl).then((res) => {
-      console.log(res);
-    });
-  },
+	name: "app",
+	mounted: function () {
+		let str = "012345678901234567890123456789";
+		let str1 = str.substr(0, 100);
+		console.log(str1.length);
+		this.axios.get(getBlogListUrl).then((res) => {
+			console.log(res);
+		});
+	},
 };
 </script>
 
